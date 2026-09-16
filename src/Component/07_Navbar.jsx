65fc517fbd673506541defbc8cd903490fd7aba9
gaurav17032007 <g65855382@gmail.com>
2026-09-16 23:38:06 +0530

    Update navbar styling and assets

--- src/Component/07_Navbar.jsx
+++ src/Component/07_Navbar.jsx
@@ -1,8 +1,12 @@
 import '../Css_files/07_Navbar.css';
 import { Link } from "react-router-dom"
-import image from '../images/logo.png';
+import image1 from '../images/logo.png';
+import image2 from '../images/Home.png';
+import image3 from '../images/Cart.png';
+import image4 from '../images/Wishlist.png';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Wish_list from './04_Wishlist';
 // import Account_img from '../images/Account.png';
 function Navbar({ setvalue }) {
     const [val, setval] = useState("");
@@ -19,32 +23,50 @@ function Navbar({ setvalue }) {
         <div className='Nav_div'>
             <nav className='Nav_link'>
                 <div className='img_span'>
-                    <img src={image} alt="" />
-                    <span>Shop<span className='Ease'>Ease</span></span>
+                    <img src={image1} alt="ShopEase logo" />
+                    <span className='Shop'>Shop<span className='Ease'>Ease</span></span>
                 </div>
+
                 <div className='Home_plus'>
-                    <Link to='/'>Home</Link>
+
+                    <div className='Home_img'>
+                        <Link to='/'>
+                            <img src={image2} alt="Home logo" />Home
+                        </Link>
+                    </div>
+
                     <Link to='/categories'>Categories</Link>
                 </div>
 
                 <div className='Nav_input'>
-                    <form onSubmit={handler}>
-                        <input type="text"
-                            placeholder='select the product'
-                            value={val}
-                            onChange={(e) => setval(e.target.value)}
-                        />
-                        <button type='submit'>Search</button>
-                    </form>
+
+                    <input type="text"
+                        placeholder='🔍 Search for products, brands and more...'
+                        value={val}
+                        onChange={(e) => setval(e.target.value)}
+                    />
+                    <button onSubmit={handler}>Search</button>
                 </div>
 
                 <div className='Wishlist_plus'>
-                    <Link to='/wish_list'>Wishlist</Link>
-                    <Link to='/cart'>Cart</Link>
+
+                    <Link to='/wish_list'>
+                        <img src={image4} alt="" />
+                        Wishlist
+                    </Link>
+
+                    <div className='Cart_img'>
+
+                        <Link to='/cart'>
+                            <img src={image3} alt="" />Cart
+                        </Link>
+
+                    </div>
+
                     <Link to='/account'>Account</Link>
                 </div>
                 {/* <img src={Account_img} alt="" /> */}
-        </nav>
+            </nav>
         </div >
     )
 }
