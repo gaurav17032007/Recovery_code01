2c49656b8819987ca2397f8cfb93510c6ec6f2dd
gaurav17032007 <g65855382@gmail.com>
2026-09-21 21:04:42 +0530

    Add product image carousel controls

--- src/Component/01_Home.jsx
+++ src/Component/01_Home.jsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from 'react-router-dom';
 function Home() {
     const [product, setprodeuct] = useState(null);
+    const [Index, setIndex] = useState(0);
     const navigator = useNavigate();
     useEffect(() => {
         const call = async function () {
@@ -17,12 +18,38 @@ function Home() {
         console.log("Image thubnail run");
         navigator('/Product_item');
     }
+
+    const images_slice = product?.slice(10, 15) || [];
+    const images_list = images_slice.map((item) => item.images[0]);
+    const images_list1= images_list.map((item)=> [item.images,item.images,item.images])
+    function left(e) {
+        e.preventDefault();
+        let newIndex = Index - 1;
+        if (newIndex < 0) {
+            newIndex = images_list1.length - 1;
+        }
+        setIndex(newIndex);
+    }
+
+    function right(e) {
+        e.preventDefault();
+        let newIndex = Index + 1;
+        if (newIndex >= images_list1.length) {
+            newIndex = 0;
+        }
+        setIndex(newIndex);
+    }
     return (
         <div>
-            <h1>Home page</h1>
-            {product?.slice(10, 14).map((item) => (
+            <div className=''>
+                <button onClick={left}>{"<"}</button>
+                <img src={images_list1[Index]} width="200" alt="product" />
+                <button onClick={right}>{">"}</button>
+            </div>
+            {product?.slice(10, 14).map((item) => {
                 <img src={item.images?.[0]} alt="" />
-            ))}
+            })
+            }
 
 
             {/* {product?.filter((index) =>
