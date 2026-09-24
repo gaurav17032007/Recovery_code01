f4e0c3cd04234b0b753e8f1ab79208c15fd871b4
gaurav17032007 <g65855382@gmail.com>
2026-09-24 23:54:23 +0530

    Update e-commerce project and practice script

--- React_Project/E-commerce_project/pro1.js
+++ React_Project/E-commerce_project/pro1.js
@@ -1,1 +1,8 @@
-console.log(2/6);
+let arr=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+let getproduct=[];
+for(let i=0;i<arr.length;i+=3){
+    getproduct.push(arr.slice(i,i+3));
+}
+getproduct.slice(0,5).map((item,j)=>console.log(item));
+
+// console.log(getproduct);
